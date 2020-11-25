75401fe8bd70428d2a2483a16010bce4e9f1acae
Mohit Singh <mohit@mohitsingh.in>
2020-11-25 09:11:15 +0530

    Make patch reliable

--- builder.sh
+++ builder.sh
@@ -1,20 +1,17 @@
 #/usr/bin/env bash
-# clone youtube-dl crap
 git clone --depth=1 https://github.com/ytdl-org/youtube-dl.git crap
-# apply minimize patch
+apply patches/minimize patch
 cd crap
-git apply "../minimize.patch"
-# clean thing up
+git apply "../patches/minimize.patch"
 cd youtube_dl/extractor
 mkdir tmp
-mv __init__.py common.py commonmistakes.py commonprotocols.py extractors.py youtube.py tmp
+mv __init__.py common.py commonmistakes.py commonprotocols.py youtube.py tmp
 rm *.py
 mv tmp/* .
+cp ../../../patches/extractors.py .
 rm -rf tmp
-# create our beautiful ytd out of it
 cd ../..
 make
 mv youtube-dl ../ytd
 cd ..
-# kick the crap out
 rm -rf crap
---
+++ patches/extractors.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+from __future__ import unicode_literals
+
+from .youtube import (
+    YoutubeIE,
+    YoutubeFavouritesIE,
+    YoutubeHistoryIE,
+    YoutubeTabIE,
+    YoutubePlaylistIE,
+    YoutubeRecommendedIE,
+    YoutubeSearchDateIE,
+    YoutubeSearchIE,
+    # YoutubeSearchURLIE,
+    YoutubeSubscriptionsIE,
+    YoutubeTruncatedIDIE,
+    YoutubeTruncatedURLIE,
+    YoutubeYtUserIE,
+    YoutubeWatchLaterIE,
+)
